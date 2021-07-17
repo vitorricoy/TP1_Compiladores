428f198ffd6d6d03a31b6fa9c19c7b1baa9a1daa
Vitor Rodarte <vitorricoy@gmail.com>
2021-07-17 18:59:24 -0300

    Correções de compilação

--- include/conversor_instrucoes.h
+++ include/conversor_instrucoes.h
@@ -7,8 +7,8 @@
 #include "tabela_simbolos.h"
 
 namespace Conversor {
-    int converterOperando(std::string operando, TabelaSimbolos& tabela);
-    std::vector<int> converterInstrucao(std::vector<std::string> instrucao, TabelaSimbolos& tabela, int linhaAtual);
+    int converterOperando(std::string operando, TabelaSimbolos& tabela, int& linhaAtual);
+    std::vector<int> converterInstrucao(std::vector<std::string> instrucao, TabelaSimbolos& tabela, int& linhaAtual);
 }
 
 #endif
