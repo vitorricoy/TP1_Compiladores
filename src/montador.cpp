3914bc9f6f1e6c910f7e285949c30bfb626d50ef
Vitor <assuncao26vitor@gmail.com>
2021-07-17 16:48:03 -0300

    Montador Passo Um

--- src/montador.cpp
+++ src/montador.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+#include "util.h"
 #include "montador.h"
 
-void Montador::Montador(std::string arquivoPrograma) {
+Montador::Montador(std::string arquivoPrograma) {
     this->leitorArquivos = new LeitorArquivos(arquivoPrograma);
 }
 
-void Montador::executarPassoUm() {
+std::vector<std::vector<std::string>> Montador::executarPassoUm() {
 
+    std::vector<std::vector<std::string>> resultado;
+
+    std::vector<std::string> palavras;
+
+    int numeroLinha = 0;
+
+    while(!leitorArquivos.arquivoLidoAteFim()) {
+        std::string linha = this->leitorArquivos.obterProximaLinha();
+
+        linha = removeComentario(linha);
+
+        palavras = separaPalavras(linha);
+
+        if(palavras[0].back() == ':') {
+            tabelaSimbolos.salvarSimbolo(palavras[0].substr(0, palavras[0].size()-1), numeroLinha);
+            palavras.erase(palavras.begin());
+        }
+
+        for(int i=0; i<palavras.size(); i++) {
+            if(palavras[i] == "WORD") {
+                palavras.erase(palavras.begin() + i);
+            }
+        }
+        
+        if(palavras[0] == "END") {
+            break;
+        }
+
+        numeroLinha += 1;
+
+        resultado.push_back(palavras);
+    }
+
+    return resultado;
 }
 
 void Montador::executarPassoDois() {
--- src/util.cpp
+++ src/util.cpp
@@ -1,2 +1,30 @@
 #include "util.h"
+#include <string>
+#include <sstream>
+#include <vector>
 
+std::vector<std::string> separaPalavras(std::string linha) {
+    std::istringstream streamLinha(linha);
+    std::string palavra;
+    std::vector<std::string> palavras;
+
+    while (streamLinha >> palavra) {
+        palavras.push_back(palavra);
+    }
+
+    return palavras;
+}
+
+std::string juntaPalavras(std::vector<std::string> linha) {
+    std::string join("");
+
+    for (std::string palavra: linha) {
+        join = join + palavra + " ";
+    }
+
+    return join;
+}
+
+std::string removeComentario(std::string linha) {
+    return linha.substr(0, linha.find(";"));
+}
